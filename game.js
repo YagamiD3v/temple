dcbbaaa4f1aaf6de241ed66e5c39cc7ae4930f0e
YagamiD3v <129217539+YagamiD3v@users.noreply.github.com>
2026-06-07 00:41:33 +0200

    Update page démo

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"895308e9-55c7-430d-8106-8431c5b8c36b","remote_package_size":3097792,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":3097792,"audio":false}]});
+  loadPackage({"package_uuid":"d7abc5ce-7689-4e89-8224-ec4616bff1e4","remote_package_size":9924772,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":9924772,"audio":false}]});
 
 })();
